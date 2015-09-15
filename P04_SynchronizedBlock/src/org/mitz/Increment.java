3ea6812a2f29b30346b8dc52ae6adc5eab663615
91014863 <91014863@172.18.25.152>
2015-09-15 15:03:22 +0530

    Stage 04 Using the 'synchronized block' technique to resolve time issue as well as parallel modification of instance variables with separate method.

--- P04_SynchronizedBlock/src/org/mitz/Increment.java
+++ P04_SynchronizedBlock/src/org/mitz/Increment.java
@@ -5,6 +5,9 @@ public class Increment implements Processor{
 	private int counter1;
 	private int counter2;
 	
+	private Object obj1 = new Object();
+	private Object obj2 = new Object();
+	
 	public void process(){
 		for (int i = 0; i < 100; i++) {
 			increment1();
@@ -12,14 +15,18 @@ public class Increment implements Processor{
 		}
 	}
 	
-	public synchronized void increment1(){
+	public void increment1(){
 		sleep(1);
-		counter1++;
+		synchronized (obj1) {
+			counter1++;
+		}
 	}
 	
-	public synchronized void increment2(){
+	public void increment2(){
 		sleep(1);
-		counter2++;
+		synchronized (obj2) {
+			counter2++;
+		}
 	}
 	
 	private void sleep(int time) {
