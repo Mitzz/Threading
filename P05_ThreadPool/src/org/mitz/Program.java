0ddb01364953480e847ca92e01994df6357f9fd4
91014863 <91014863@172.18.25.152>
2015-09-15 15:26:33 +0530

    Stage 02 Sending message 'process' to the same Processor Object numerous times using threading with 'synchronize block' technique

--- P05_ThreadPool/src/org/mitz/Program.java
+++ P05_ThreadPool/src/org/mitz/Program.java
@@ -11,11 +11,19 @@ public class Program {
 		
 		Thread t1 = new Thread(worker);
 		Thread t2 = new Thread(worker);
+		Thread t3 = new Thread(worker);
+		Thread t4 = new Thread(worker);
+		Thread t5 = new Thread(worker);
+		Thread t6 = new Thread(worker);
 		
 		t1.start(); t2.start();
+		t3.start(); t4.start();
+		t5.start(); t6.start();
 		
 		try {
 			t1.join(); t2.join();
+			t3.join(); t4.join();
+			t5.join(); t6.join();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
